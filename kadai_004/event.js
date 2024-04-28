ceffeeb1edd7423b2f01d13b06ad45b15364fd2f
mogimogiko <moemmmoe66@gmail.com>
2024-04-28 13:06:22 +0900

    説明を追加

--- kadai_004/event.js
+++ kadai_004/event.js
@@ -1,8 +1,10 @@
 $(function () {
   $(window).on({
+    // 読み込み
     'load': function () {
       console.log('loadイベントが発生しました');
     },
+    // スクロール
     'scroll': function () {
       console.log('scrollイベントが発生しました');
     }
