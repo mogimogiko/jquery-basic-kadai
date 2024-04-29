4dc5c4f8b7ae8a8600337774b4934346f88989cf
mogimogiko <moemmmoe66@gmail.com>
2024-04-29 17:38:33 +0900

    変更

--- kadai_004/event.js
+++ kadai_004/event.js
@@ -1,12 +1,7 @@
-$(function () {
-  $(window).on({
-    // 読み込み
-    'load': function () {
-      console.log('loadイベントが発生しました');
-    },
-    // スクロール
-    'scroll': function () {
-      console.log('scrollイベントが発生しました');
-    }
-  })
+$(window).on('load',function () {
+  console.log('loadイベントが発生しました');
 });
+
+$(window).on('scroll',function () {
+  console.log('scrollイベントが発生しました');
+})
